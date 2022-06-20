35f29b0dbb1d67ba6b5f52e729cd5ca17cb07639
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-06-20 09:41:55 +0530

    Update init.py

--- TestAction/Lambda/init.py
+++ TestAction/Lambda/init.py
@@ -1,1 +1,24 @@
 
+from .aws_lambda import (
+    deploy,
+    deploy_s3,
+    invoke,
+    init,
+    build,
+    upload,
+    cleanup_old_versions,
+)
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+
+logging.getLogger(__name__).addHandler(NullHandler())
